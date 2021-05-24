2511b5f8fe21f8346091cdd424f0e77f57238bb2
朱烨 <1359639620@qq.com>
2021-05-24 14:03:39 +0800

    vue创建的案例

---
+++ demo/src/components/HelloWorld.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank"> Core Docs </a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank"> Forum </a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank"> Community Chat </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank"> Twitter </a>
+      </li>
+      <br />
+      <li>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank"> vue-router </a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank"> vuex </a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank"> vue-loader </a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+    <h3>这是一个案例</h3>
+    <div class="box"></div>
+    <div ref="chart" style="width: 100%; height: 376px"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+    mounted(){
+    this.getEchartData()  
+    },
+     methods: {
+    getEchartData() {
+      const chart = this.$refs.chart
+      if (chart) {
+        const myChart = this.$echarts.init(chart)
+        const option = {
+           tooltip: {
+        trigger: 'item'
+    },
+    legend: {
+        top: '5%',
+        left: 'center'
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+                show: false,
+                position: 'center'
+            },
+            emphasis: {
+                label: {
+                    show: true,
+                    fontSize: '40',
+                    fontWeight: 'bold'
+                }
+            },
+            labelLine: {
+                show: false
+            },
+            data: [
+                {value: 1048, name: '搜索引擎'},
+                {value: 735, name: '直接访问'},
+                {value: 580, name: '邮件营销'},
+                {value: 484, name: '联盟广告'},
+                {value: 300, name: '视频广告'}
+            ]
+        }
+    ]
+        }
+        myChart.setOption(option)
+        window.addEventListener("resize", function() {
+          myChart.resize()
+        })
+      }
+       this.$on('hook:destroyed',()=>{
+         window.removeEventListener("resize", function() {
+          myChart.resize();
+        });
+        })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+h3 {
+  font-size: 16px;
+  color: #42b983;
+}
+.box {
+  width: 200px;
+  height: 200px;
+  background: #42b983;
+}
+</style>
